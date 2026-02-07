ec67f16e32bdd851e769b2a2448fdb5f8216feeb
Paulina Gutierrez <paulinagutierrez.ucae@gmail.com>
2026-02-06 22:38:53 -0300

    Add comment for global CSS import in main.tsx
    Add a comment about global CSS import.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// Si tienes CSS global, descomenta la siguiente línea:
+// import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
